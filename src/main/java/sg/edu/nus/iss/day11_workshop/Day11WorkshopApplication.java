4dca78e5358b7d9259365f658e1c6a72f0aef0a5
Shiyi <yeoshiyi22@gmail.com>
2023-02-14 15:47:49 +0800

    updated main app

--- src/main/java/sg/edu/nus/iss/day11_workshop/Day11WorkshopApplication.java
+++ src/main/java/sg/edu/nus/iss/day11_workshop/Day11WorkshopApplication.java
@@ -1,13 +1,41 @@
 package sg.edu.nus.iss.day11_workshop;
 
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Day11WorkshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Day11WorkshopApplication.class, args);
-	}
+	private static final Logger logger = LoggerFactory.getLogger(Day11WorkshopApplication.class); 
+	private static String portNumber = null;
+	private static final String DEFAULT_PORT = "3000"; 
 
+	public static void main(String[] args) {
+
+		// option 1: mvn spring-boot:run -Dspring-boot.run.arguments=--port=3050
+		for (String value : args) {
+			logger.debug("value : " + value);
+			if (value.contains("--port=")) {
+				portNumber = value.substring(value.length() - 4, value.length()); 
+				logger.debug("portNumber : " + portNumber);
+			}
+		}
+		// option 2
+		if (portNumber == null) {
+			portNumber = System.getenv("PORT"); 
+			logger.debug("System environment portNumber : " + portNumber); 
+		}
+		// default option
+		if (portNumber == null) {
+			portNumber = DEFAULT_PORT; 
+		}
+
+		SpringApplication app = new SpringApplication(Day11WorkshopApplication.class); 
+		app.setDefaultProperties(Collections.singletonMap("server.port", portNumber));
+		app.run(args); 
+	}
 }
